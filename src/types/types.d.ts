0774022b39c1e140266e9a0677008362b5d3e90e
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2021-07-20 13:08:03 +0200

    bug: types gitignore

--- src/types/types.d.ts
+++
@@ -1,3 +0,0 @@
-export declare type MarkRequired<T, ReqK extends keyof T> = Exclude<T, ReqK> &
-  Required<Pick<T, ReqK>>;
-export declare type MarkOptional<T, OptK extends keyof T> = Omit<T, OptK> & Partial<Pick<T, OptK>>;
