6d9aeeb3c5a84b109775511e7a5fc934446ca6dc
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2021-08-17 16:35:48 +0200

    fix(user): Fix URL path

--- src/resource/user/api.ts
+++ src/resource/user/api.ts
@@ -5,6 +5,6 @@ import { UserView } from "./types";
 export namespace Users {
   /** Non-exposed API from Laces. */
   export async function GetUser(userId: string) {
-    return await Laces.API.asJSON<UserView>(`/users/${userId}`);
+    return await Laces.API.asJSON<UserView>(`/user/${userId}`);
   }
 }
